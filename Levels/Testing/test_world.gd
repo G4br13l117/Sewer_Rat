e777c6854456d644cbd139958d747be78469d225
Gabriel <GabrielOrtega117@Gmail.com>
2024-08-02 23:56:09 -0400

    Player Animations
    Configured player movement and animations.
    Tested with Camera limits (incomplete).

---
+++ Levels/Testing/test_world.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SetCameraLimits()
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	#SetCameraLimits()
+	pass
+	
+	
+func SetCameraLimits():
+	var FocusedCollisionArea = get_node("Main_Level/RightCollisionShape2D")
+	var AreaHeightLimit = FocusedCollisionArea.get_shape().size.y
+	#print(("AreaHeightLimit: %s") % AreaHeightLimit)
+	var AreaWidthLimit = FocusedCollisionArea.get_shape().size.x
+	#print(("AreaWidthLimit: %s") % AreaWidthLimit)
+	var AreaPosition = FocusedCollisionArea.position
+	#var mapCellSize = $TileMap.cellSize
+	$Player/Camera2D.limit_left = AreaPosition.x - (AreaWidthLimit/2)
+	#print(("Left limit: %s") % $Player/Camera2D.limit_left)
+	$Player/Camera2D.limit_right = AreaPosition.x + (AreaWidthLimit/2)
+	print($Player/Camera2D.limit_right)
+	$Player/Camera2D.limit_top = AreaPosition.y - (AreaHeightLimit/2)
+	print($Player/Camera2D.limit_top)
+	$Player/Camera2D.limit_bottom = AreaPosition.y + (AreaHeightLimit/2)
+	print($Player/Camera2D.limit_bottom)
+	#print(AreaPosition)
--- Player.gd
+++ Player.gd
@@ -1,106 +1,52 @@
-#extends CharacterBody2D
-#
-#var movement = Vector2()
-#var speed = 100
-#var jumpHeight = 600
-#var fallVelocity = 5
-#
-#@onready var defaultAnimation = $AnimatedSprite2D
-#func _ready():
-	#pass # Replace with function body.
-#
-#
-#func _physics_process(delta):
-	#PlayerMovement()
-	#
-	#movement = movement.normalized() * speed * delta
-	#move_and_slide()
-	#
-#
-#func PlayerMovement():
-	#var Left = Input.is_action_pressed("ui_left")
-	#var Right = Input.is_action_pressed("ui_right")
-	#var Jump = Input.is_action_pressed("ui_accept")
-	#
-	#movement.x = -int(Left) + int(Right)
-	#movement.y = -int(Jump)
-	#
-	#if movement.x != 0:
-		#velocity.x = movement.x * speed
-	#else:
-		#velocity.x = 0
-	#
-#
-#func PlayerAnimation():
-	#pass
-	#
-#
-#func CurrentGravity():
-	#var newGravity = gravity_force.new()
-	#velocity.y = fallVelocity
-	#if !is_on_floor():
-		#fallVelocity += newGravity.gravityStrength
-	#else:
-		#fallVelocity = 5
-		#
-	#print(fallVelocity)
-#
-#func PlayerWeaponFire():
-	#pass
-	#
-######################
 extends CharacterBody2D
 
+@export var Peakrange = 30
 @export var SPEED = 300.0
 var JUMP_VELOCITY = SPEED * -0.75 #-400.0
 #var Health = 10
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var gravity = SPEED #ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var anim = get_node("AnimationPlayer")
+@onready var anim = get_node("AnimatedSprite2D")
+#@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
-	# Add the gravity.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if not is_on_floor() and velocity.y < gravity:
-		#move_toward(velocity.y,gravity,delta)
 		velocity.y += gravity * delta
-	#else: velocity.y == 0
 
-	#if get_node("AnimatedSprite2D").animation != "Die":
-	if $AnimatedSprite2D.animation != "hurt":
+	#if $AnimatedSprite2D.animation != "Hurt":
+	if anim.animation != "Hurt":
 
+		if direction < 0: # == -1:
+			anim.flip_h = true
+		elif direction > 0: # == 1:
+			anim.flip_h = false
 
 		# Handle jump.
 		if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
-			#print(Input.get_action_strength("ui_left") - Input.get_action_strength("ui_left"))
 			velocity.y = JUMP_VELOCITY
-			#if velocity.y < 0:
-				##anim.play("Jump")
-				#$AnimationPlayer.play("jump")
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction < 0: # == -1:
-			get_node("AnimatedSprite2D").flip_h = true
-		elif direction > 0: # == 1:
-			get_node("AnimatedSprite2D").flip_h = false
+		if not is_on_floor():
+			if (velocity.y >= -Peakrange) and (velocity.y <= Peakrange):
+				anim.play("Jump_Peak")
+			elif velocity.y < -Peakrange:
+				anim.play("Jump_Rising")
+			elif velocity.y > Peakrange:
+				anim.play("Jump_Falling")
 
 		if direction:
 			velocity.x = direction * SPEED
-			#if velocity.y == 0: # and velocity.x != 0:
-				#anim.play("Run")
+			if is_on_floor():
+				anim.play("Run")
 				#$AnimationPlayer.play("run")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			if velocity.y == 0: # and velocity.x == 0:
-				#anim.play("Idle")
-				$AnimationPlayer.play("Idle")
+			if velocity.y == 0 and is_on_floor():
+				anim.play("Idle")
+				#$AnimationPlayer.play("Idle")
 			
-		#if velocity.y > 0:
-			#anim.play("Fall")
-			#$AnimationPlayer.play("fall")
 
 		move_and_slide()
 
